b051a07d57e3df59eafac517bb850891e0bed52f
imaimai17468 <tosiki_801_life@outlook.jp>
2022-11-12 21:15:26 +0900

    initial commit

---
+++ go/cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+func main() {
+	endpoint := "localhost:9000"
+	rootUser := "nutmeg"
+	rootPassword := "gidaifes"
+	useSSL := true
+
+	// Initialize minio client object.
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(rootUser, rootPassword, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("%#v\n", minioClient) // minioClient is now setup
+}
