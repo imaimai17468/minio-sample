17411ca5a3212f1ee8dbd218da2120e31c39acc0
imaimai17468 <tosiki_801_life@outlook.jp>
2023-02-11 20:57:49 +0900

    [feat] 画像が保存できるようにする

--- next/src/pages/api/image.ts
+++ next/src/pages/api/image.ts
@@ -1,6 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { formidable } from "formidable";
 
 const Minio = require("minio");
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
 export default async function handler(
   req: NextApiRequest,
@@ -15,10 +22,37 @@ export default async function handler(
   });
 
   if (req.method === "POST") {
-    const bucketName = req.body.bucketName;
-    const fileName = req.body.fileName;
-    const file = req.body.file;
+    console.log("POST")
 
-    const post = await minioClient.fPutObject(bucketName, 'QR_396974', '/data', file);
+    const form = formidable();
+    const fs = require("fs");
+
+    form.parse(req, async (err, fields, files: any) => {
+      if (err) {
+        console.error("Error", err);
+        throw err;
+      }
+
+      const bucketName = fields.bucketName;
+      const fileName = fields.fileName;
+      const metaData = {
+        "Content-Type": "image/png",
+      };
+
+      console.log("bucketName", bucketName)
+      console.log("fileName", fileName)
+      console.log("metaData", metaData)
+
+      try {
+        const res = await minioClient.putObject(
+          bucketName,
+          fileName,
+          fs.createReadStream(files.file.filepath),
+          metaData
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    });
   }
 }
